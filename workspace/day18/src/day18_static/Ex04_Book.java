4f04df6bed94041341fae29b09da07054767deea
TAEHYUN LEE <yeourain@gmail.com>
2023-05-15 21:39:57 +0900

    Static Block Error

---
+++ workspace/day18/src/day18_static/Ex04_Book.java
@@ -0,0 +1,13 @@
+package day18_static;
+
+	class Book {
+		// Field
+		// Static 필
+		static int cnt = 0; // new Book() 이 호출될 때 마다 1씩 증가되는 변수
+		
+		// 인스턴스 필드 (non-static)
+	}
+
+public class Ex04_Book {
+
+}
